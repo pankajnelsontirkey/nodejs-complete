67112c6f960aa3e92ddd446afb05428139135327
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-02-27 21:55:16 +0530

    commit auth middleware;

---
+++ graphql-api/backend/middleware/auth.js.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = require('../utils/constants');
+
+const auth = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    req.isAuth = false;
+    return next();
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  let decodedToken;
+
+  try {
+    decodedToken = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    req.isAuth = false;
+    return next();
+  }
+
+  if (!decodedToken) {
+    req.isAuth = false;
+    return next();
+  }
+
+  req.userId = decodedToken.userId;
+  req.isAuth = true;
+
+  return next();
+};
+
+module.exports = auth;
