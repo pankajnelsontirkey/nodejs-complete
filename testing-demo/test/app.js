bd60610170f9518e557b44919ab09b6f2396b507
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-03-11 14:50:56 +0530

    added mocha, chai; wrote simple test;

---
+++ testing-demo/test/app.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+
+it('should add two numbers', function () {
+  const num1 = 2;
+  const num2 = 3;
+
+  expect(num1 + num2).to.equal(5);
+});
+
+it('should not give sum of two numbers to 6', function () {
+  const num1 = 3;
+  const num2 = 3;
+
+  expect(num1 + num2).not.to.equal(6);
+});
