d158afc636227c3309073c51983c69f6094e8c2c
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-03-12 22:20:55 +0530

    added simple route handler;

--- es-modules/app.js
+++ es-modules/app.js
@@ -1,13 +1,10 @@
 const fs = require('fs');
 
 const express = require('express');
+const handler = require('./handler');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  fs.readFile('my-page.html', 'utf8', (err, data) => {
-    res.send(data);
-  });
-});
+app.get('/', handler);
 
 app.listen(3000);
